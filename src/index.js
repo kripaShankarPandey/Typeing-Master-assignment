d99976c2bd1e7cc84e9319f76c9610a57071ed83
Kripa Shankar Pandey <105850915+kripaShankarPandey@users.noreply.github.com>
2023-05-27 13:40:18 +0530

    providing store data to all components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
