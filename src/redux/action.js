b41f3bd42e77a808517d88d33c04d8498b9b8cd8
Kripa Shankar Pandey <105850915+kripaShankarPandey@users.noreply.github.com>
2023-05-27 13:57:49 +0530

    Update action.js

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,14 +1,17 @@
 import { CORECT_KEY_COUNT, KEY_COUNT, RESET, SHOW_RESULTS } from "./actionType"
-
+// To reset application
 export const resetAction=()=>{
 return {type:RESET}
 }
+ //To counting number of keys pressed
 export const keyCountAction=()=>{
 return {type:KEY_COUNT}
 }
+ // To counting correct number of keys pressed
 export const correctKeyCountAction=()=>{
 return {type:CORECT_KEY_COUNT}
 }
+ //To show the results
 export const showResultsAction=()=>{
 return {type:SHOW_RESULTS}
 }
