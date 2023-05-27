bcf1b044a964601c536fc35b5e2952043d99cbf7
Kripa Shankar Pandey <105850915+kripaShankarPandey@users.noreply.github.com>
2023-05-27 13:52:21 +0530

    store file added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
 import { legacy_createStore } from "redux";
 import { reducer } from "./reducer";
-
+//exporting store data so each components can use store data
 export const store=legacy_createStore(reducer)
